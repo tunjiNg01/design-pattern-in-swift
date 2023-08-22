b9583576fcd088d7ae92dd3b7c7a94a861c35aea
tunjiNg01 <nahtlosng@gmail.com>
2023-08-22 12:35:30 +0100

    facade design pattern

--- MyPlayground.playground/Pages/FacadeDesignPattern.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/FacadeDesignPattern.xcplaygroundpage/Contents.swift
@@ -2,6 +2,53 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// Subsystems
+
+class CPU {
+    func freeze() { /* Implementation */ }
+    func jump(position: Int) { /* Implementation */ }
+    func execute() { /* Implementation */ }
+}
+
+class Memory {
+    func load(position: Int, data: String) { /* Implementation */ }
+}
+
+class HardDrive {
+    func read(position: Int, size: Int) -> String {
+        return "Reading the hard drive with size: \(size), and position: \(position)"
+    }
+}
+
+
+// Facade
+class ComputerFacade {
+    private let cpu: CPU
+    private let memory: Memory
+    private let hardDrive: HardDrive
+    
+    init() {
+        self.cpu = CPU()
+        self.memory = Memory()
+        self.hardDrive = HardDrive()
+    }
+    
+    func start() {
+        cpu.freeze()
+        memory.load(position: 0, data: "Boot data")
+        cpu.jump(position: 0)
+        cpu.execute()
+    }
+    
+    func fetchData() -> String {
+        return hardDrive.read(position: 0, size: 1024)
+    }
+}
+
+// Client Code
+let computer = ComputerFacade()
+computer.start()
+let data = computer.fetchData()
+print("Result: \(data)")
 
 //: [Next](@next)
